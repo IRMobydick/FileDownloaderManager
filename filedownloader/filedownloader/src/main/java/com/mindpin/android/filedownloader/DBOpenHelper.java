474fe339e90b0747eef11c40980deac860773fd9
MAY3AM <pink_mobydick@yahoo.com>
2015-04-13 21:24:52 +0430

    DBOpenHelper.java
    Update

--- filedownloader/filedownloader/src/main/java/com/mindpin/android/filedownloader/DBOpenHelper.java
+++ filedownloader/filedownloader/src/main/java/com/mindpin/android/filedownloader/DBOpenHelper.java
@@ -10,16 +10,16 @@ public class DBOpenHelper extends SQLiteOpenHelper {
     private static final int VERSION = 1;
 
     public DBOpenHelper(Context context) {
-
         super(context, DBNAME, null, VERSION);
-        Log.i("调试db 0", "true");
+        Log.i("Debugging db 0", "true");
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        Log.i("调试db 1", "true");
+        Log.i("Debugging db 1", "true");
         db.execSQL("CREATE TABLE IF NOT EXISTS filedownlog (id integer primary key autoincrement, downpath varchar(100), threadid INTEGER, downlength INTEGER)");
-        // 这里的 status 字段用来表示暂停或者正在下载, 暂停: pause, 正在下载: download
+
+        // Status field is used to indicate where the pause or is downloading, pause: pause, downloading: download
         db.execSQL("CREATE TABLE IF NOT EXISTS filesizelog (downpath varchar(100), filesize INTEGER)");
     }
 
@@ -28,7 +28,7 @@ public class DBOpenHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS filedownlog");
         db.execSQL("DROP TABLE IF EXISTS filesizelog");
         onCreate(db);
-        Log.i("调试db 2", "true");
+        Log.i("Debugging db 2", "true");
     }
 
 }
